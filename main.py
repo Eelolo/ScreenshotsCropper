cee40dfd373f62bb0ec8283715d978d1d18ec9dc
Eelolo <Eelolo@yandex.ru>
2021-04-21 20:25:38 +0300

    folding to tray functionality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
-from PIL import ImageGrab, ImageTk
+from PIL import ImageGrab, ImageTk, Image
+from pystray import MenuItem, Icon
+import keyboard
 
 
 class App(Frame):
@@ -7,15 +9,36 @@ class App(Frame):
         self.root = root
         Frame.__init__(self, root)
 
+        self.icon = None
+        self.icon_img = Image.open("ico.png")
+
+        self.configure_root()
         self.create_widgets()
         self.draw_widgets()
-        self.grab_image()
+
+    def configure_root(self):
+        self.root.protocol('WM_DELETE_WINDOW', self.withdraw_window)
+
+    def quit_window(self):
+        self.icon.stop()
+        self.root.destroy()
+
+    def show_window(self):
+        if self.icon:
+            self.icon.stop()
+            self.root.after(0, self.root.deiconify)
+
+    def withdraw_window(self):
+        self.root.withdraw()
+        menu = (MenuItem('Show', self.show_window), MenuItem('Quit', self.quit_window))
+        self.icon = Icon("ScreenshotsCutter", self.icon_img, "Screenshots\nCutter", menu)
+        self.icon.run()
 
     def create_widgets(self):
-        self.image_canv = Canvas(self, width=1920, height=1080)
+        self.lbl = Label(self, text='There is no image here.\nUse the Print Screen button.')
 
     def draw_widgets(self):
-        self.image_canv.pack()
+        self.lbl.pack()
 
     def run(self):
         self.mainloop()
@@ -23,8 +46,10 @@ class App(Frame):
     def grab_image(self):
         self.screenshot = ImageTk.PhotoImage(ImageGrab.grabclipboard())
 
+
 if __name__ == '__main__':
     root = Tk()
     app = App(root)
     app.pack()
+    keyboard.add_hotkey('Print Screen', app.show_window)
     app.run()
