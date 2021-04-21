4a317a692fc35a84dbc5d69ce7fe584fe762f78f
Eelolo <Eelolo@yandex.ru>
2021-04-21 18:31:56 +0300

    getting image from clipboard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import ImageGrab, ImageTk
 
 
 class App(Frame):
@@ -6,8 +7,12 @@ class App(Frame):
         self.root = root
         Frame.__init__(self, root)
 
+        self.create_widgets()
+        self.draw_widgets()
+        self.grab_image()
+
     def create_widgets(self):
-        self.image_canv = Canvas(self)
+        self.image_canv = Canvas(self, width=1920, height=1080)
 
     def draw_widgets(self):
         self.image_canv.pack()
@@ -15,8 +20,11 @@ class App(Frame):
     def run(self):
         self.mainloop()
 
+    def grab_image(self):
+        self.screenshot = ImageTk.PhotoImage(ImageGrab.grabclipboard())
 
 if __name__ == '__main__':
     root = Tk()
     app = App(root)
+    app.pack()
     app.run()
