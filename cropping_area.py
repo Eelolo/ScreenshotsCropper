17db23610f59e1f8e30f4b24550bbd3d2c3155a0
Eelolo <Eelolo@yandex.ru>
2021-04-26 20:34:12 +0300

    angle handles outer limits

--- cropping_area.py
+++ cropping_area.py
@@ -226,75 +226,79 @@ class Cropping_area:
 
     @update_area_coords
     def upper_left_handle_move(self, event):
+        x0, y0, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5 =  self.canvas.coords('upper_left_handle')
+
         diff_x = event.x - self.angle_move_start_coords.x
         diff_y = event.y - self.angle_move_start_coords.y
 
-        x0, y0, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5 =  self.canvas.coords('upper_left_handle')
-        self.canvas.coords(
-            'upper_left_handle',
-            x0 + diff_x, y0 + diff_y, x1 + diff_x, y1 + diff_y,
-            x2 + diff_x, y2 + diff_y, x3 + diff_x, y3 + diff_y,
-            x4 + diff_x, y4 + diff_y, x5 + diff_x, y5 + diff_y
-        )
+        if x0 + diff_x >= 0 and y0 + diff_y >=0:
+            self.canvas.coords(
+                'upper_left_handle',
+                x0 + diff_x, y0 + diff_y, x1 + diff_x, y1 + diff_y,
+                x2 + diff_x, y2 + diff_y, x3 + diff_x, y3 + diff_y,
+                x4 + diff_x, y4 + diff_y, x5 + diff_x, y5 + diff_y
+            )
+            self.angle_move_start_coords = event
 
-        self.angle_move_start_coords = event
-
-        self.move_vert(event, 'upper_handle')
-        self.move_hor(event, 'left_handle')
+            self.move_vert(event, 'upper_handle')
+            self.move_hor(event, 'left_handle')
 
     @update_area_coords
     def upper_right_handle_move(self, event):
+        x0, y0, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5 = self.canvas.coords('upper_right_handle')
+
         diff_x = event.x - self.angle_move_start_coords.x
         diff_y = event.y - self.angle_move_start_coords.y
 
-        x0, y0, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5 = self.canvas.coords('upper_right_handle')
-        self.canvas.coords(
-            'upper_right_handle',
-            x0 + diff_x, y0 + diff_y, x1 + diff_x, y1 + diff_y,
-            x2 + diff_x, y2 + diff_y, x3 + diff_x, y3 + diff_y,
-            x4 + diff_x, y4 + diff_y, x5 + diff_x, y5 + diff_y
-        )
+        if x0 + diff_x <= self.width and y0 + diff_y >= 0:
+            self.canvas.coords(
+                'upper_right_handle',
+                x0 + diff_x, y0 + diff_y, x1 + diff_x, y1 + diff_y,
+                x2 + diff_x, y2 + diff_y, x3 + diff_x, y3 + diff_y,
+                x4 + diff_x, y4 + diff_y, x5 + diff_x, y5 + diff_y
+            )
+            self.angle_move_start_coords = event
 
-        self.angle_move_start_coords = event
-
-        self.move_vert(event, 'upper_handle')
-        self.move_hor(event, 'right_handle')
+            self.move_vert(event, 'upper_handle')
+            self.move_hor(event, 'right_handle')
 
     @update_area_coords
     def lower_left_handle_move(self, event):
+        x0, y0, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5 = self.canvas.coords('lower_left_handle')
+
         diff_x = event.x - self.angle_move_start_coords.x
         diff_y = event.y - self.angle_move_start_coords.y
 
-        x0, y0, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5 = self.canvas.coords('lower_left_handle')
-        self.canvas.coords(
-            'lower_left_handle',
-            x0 + diff_x, y0 + diff_y, x1 + diff_x, y1 + diff_y,
-            x2 + diff_x, y2 + diff_y, x3 + diff_x, y3 + diff_y,
-            x4 + diff_x, y4 + diff_y, x5 + diff_x, y5 + diff_y
-        )
+        if x0 + diff_x >= 0 and y0 + diff_y <= self.height:
+            self.canvas.coords(
+                'lower_left_handle',
+                x0 + diff_x, y0 + diff_y, x1 + diff_x, y1 + diff_y,
+                x2 + diff_x, y2 + diff_y, x3 + diff_x, y3 + diff_y,
+                x4 + diff_x, y4 + diff_y, x5 + diff_x, y5 + diff_y
+            )
+            self.angle_move_start_coords = event
 
-        self.angle_move_start_coords = event
-
-        self.move_vert(event, 'lower_handle')
-        self.move_hor(event, 'left_handle')
+            self.move_vert(event, 'lower_handle')
+            self.move_hor(event, 'left_handle')
 
     @update_area_coords
     def lower_right_handle_move(self, event):
+        x0, y0, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5 = self.canvas.coords('lower_right_handle')
+
         diff_x = event.x - self.angle_move_start_coords.x
         diff_y = event.y - self.angle_move_start_coords.y
 
-        x0, y0, x1, y1, x2, y2, x3, y3, x4, y4, x5, y5 = self.canvas.coords('lower_right_handle')
-        self.canvas.coords(
-            'lower_right_handle',
-            x0 + diff_x, y0 + diff_y, x1 + diff_x, y1 + diff_y,
-            x2 + diff_x, y2 + diff_y, x3 + diff_x, y3 + diff_y,
-            x4 + diff_x, y4 + diff_y, x5 + diff_x, y5 + diff_y
-        )
+        if x0 + diff_x <= self.width and y0 + diff_y <= self.height:
+            self.canvas.coords(
+                'lower_right_handle',
+                x0 + diff_x, y0 + diff_y, x1 + diff_x, y1 + diff_y,
+                x2 + diff_x, y2 + diff_y, x3 + diff_x, y3 + diff_y,
+                x4 + diff_x, y4 + diff_y, x5 + diff_x, y5 + diff_y
+            )
+            self.angle_move_start_coords = event
 
-        self.angle_move_start_coords = event
-
-        self.move_vert(event, 'lower_handle')
-        self.move_hor(event, 'right_handle')
+            self.move_vert(event, 'lower_handle')
+            self.move_hor(event, 'right_handle')
 
     def angle_move_start(self, event, tag):
         coords = self.canvas.coords(tag)[0:2]
