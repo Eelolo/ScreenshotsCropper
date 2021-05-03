ccf2e25bedeafb4cc400a008ba3e31ae244da067
Eelolo <Eelolo@yandex.ru>
2021-05-03 16:24:26 +0300

    buttons icons

--- toolbar.py
+++ toolbar.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 
 class ToolBar(Frame):
@@ -6,3 +7,9 @@ class ToolBar(Frame):
         self.main = main
         Frame.__init__(self, main)
 
+        self.crop_img = ImageTk.PhotoImage(Image.open('icons/crop.png'))
+        self.arrow_img = ImageTk.PhotoImage(Image.open('icons/arrow2.png'))
+        self.diskette_img = ImageTk.PhotoImage(Image.open('icons/diskette.png'))
+        self.line_img = ImageTk.PhotoImage(Image.open('icons/line1.png'))
+        self.text_img = ImageTk.PhotoImage(Image.open('icons/text.png'))
+        self.clipboard_img = ImageTk.PhotoImage(Image.open('icons/clipboard.png'))
