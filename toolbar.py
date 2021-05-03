7e79cf3d08f9aa603920a7a09858b03a729ed77a
Eelolo <Eelolo@yandex.ru>
2021-05-03 16:54:23 +0300

    created toolbar buttons

--- toolbar.py
+++ toolbar.py
@@ -27,5 +27,42 @@ class ToolBar(Frame):
         points = get_round_rect_points(10, 10, 1355, 90, radius=25)
         self.canvas.create_polygon(points, fill=self.light_bg, smooth=True)
 
+        self.left_frame = Frame(self)
+        self.right_frame = Frame(self)
+        self.canvas.create_window(25, 25, anchor='nw', window=self.left_frame)
+        self.canvas.create_window(1190, 25, anchor='nw', window=self.right_frame)
+
+        self.crop_btn = Button(
+            self.left_frame, image=self.crop_img, height=50, width=50,
+            relief='flat', bg=self.light_bg, activebackground=self.light_bg, bd=0
+        )
+        self.text_btn = Button(
+            self.left_frame, image=self.text_img, height=50, width=50,
+            relief='flat', bg=self.light_bg, activebackground=self.light_bg, bd=0
+        )
+        self.arrow_btn = Button(
+            self.left_frame, image=self.arrow_img, height=50, width=50,
+            relief='flat', bg=self.light_bg, activebackground=self.light_bg, bd=0
+        )
+        self.line_btn = Button(
+            self.left_frame, image=self.line_img, height=50, width=50,
+            relief='flat', bg=self.light_bg, activebackground=self.light_bg, bd=0
+        )
+
+        self.clipboard_btn = Button(
+            self.right_frame, image=self.clipboard_img, height=50, width=50,
+            relief='flat', bg=self.light_bg, activebackground=self.light_bg, bd=0
+        )
+        self.diskette_btn = Button(
+            self.right_frame, image=self.diskette_img, height=50, width=50,
+            relief='flat', bg=self.light_bg, activebackground=self.light_bg, bd=0
+        )
+
     def draw_widgets(self):
         self.canvas.pack()
+        self.crop_btn.pack(side='left', ipadx=10)
+        self.text_btn.pack(side='left', ipadx=10)
+        self.arrow_btn.pack(side='left', ipadx=10)
+        self.line_btn.pack(side='left', ipadx=10)
+        self.diskette_btn.pack(side='right', ipadx=10)
+        self.clipboard_btn.pack(side='right', ipadx=10)
